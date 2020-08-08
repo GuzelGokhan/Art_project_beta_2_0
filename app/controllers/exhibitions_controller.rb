12c6e6dda5ae688ba15b2904d799f5711b210da4
GuzelGokhan <gokhan@medispring.be>
2020-08-08 14:14:57 +0200

    delete button for exhibition and show page

--- app/controllers/exhibitions_controller.rb
+++ app/controllers/exhibitions_controller.rb
@@ -1,6 +1,6 @@
 class ExhibitionsController < ApplicationController
 
-  before_action :set_galery, only: :new
+  before_action :set_exhibition, only: [:destroy, :show]
 
   def new
     @exhibition = Exhibition.new
@@ -20,14 +20,25 @@ class ExhibitionsController < ApplicationController
   end
 
   def show
-   @showroom = Showroom.new
+  
   end
+
+  def destroy
+    if @exhibition.destroy
+      flash[:success] = 'Exhibition was successfully deleted.'
+      redirect_to galery_path(@exhibition.galery)
+    else
+      flash[:error] = 'Something went wrong'
+      redirect_to exhibitions_url
+    end
+  end
+  
   
   
   private 
 
-  def set_galery
-    @galery = Galery.find(params[:galery_id])
+  def set_exhibition
+    @exhibition = Exhibition.find(params[:id])
   end
 
   def params_exhibition
