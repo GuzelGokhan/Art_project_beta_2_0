309c38c335a63919a58865bf8c465d17413b1300
GuzelGokhan <gokhan@medispring.be>
2020-08-11 20:36:41 +0200

    showroom controller + new

--- app/controllers/exhibitions_controller.rb
+++ app/controllers/exhibitions_controller.rb
@@ -1,28 +1,9 @@
 class ExhibitionsController < ApplicationController
 
-
   before_action :set_exhibition, only: [:destroy, :show]
 
-
-  def new
-    @exhibition = Exhibition.new
-  end
-  
-
-  def create
-    @exhibition = Exhibition.new(params_exhibition)
-    @exhibition.galery = @galery
-    if @exhibition.save
-      flash[:success] = "Exhibition successfully created"
-      redirect_to @exhibition
-    else
-      flash[:error] = "Something went wrong"
-      render 'new'
-    end
-  end
-
   def show
-
+    @showroom = Showroom.new
   end
 
   def destroy
@@ -36,15 +17,9 @@ class ExhibitionsController < ApplicationController
 
   end
   
-  
-  
   private 
 
   def set_exhibition
     @exhibition = Exhibition.find(params[:id])
-  end
-
-  def params_exhibition
-    params.require(:exhibition).permit(:name, :start_date, :end_date, :galery_id)
   end
 end
