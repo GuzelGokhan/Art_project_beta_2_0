1229b5ca718796bb82d46bad26332dc802ce9f99
george kosmopoulos <gkosmo1@hotmail.com>
2020-08-15 12:34:12 +0200

    putting on some meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  # def default_url_options
+  #   { host: ENV["DOMAIN"] || "localhost:3000" }
+  #  good if you set this on heroku :
+  #  heroku config:set DOMAIN=www.my_product.com
+  # end
 end
