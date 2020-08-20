7d098c8d8dbf2367e43687fa6f64ba05cb0c3e61
Sabina Pirvan <sabina2001ro@hotmail.com>
2020-08-20 20:48:50 +0200

    installed faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "faker"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "starting the seed"
+puts Faker::Name.name
+# sabina = User.create(email: "example@test.com", password: "AZERTY")
+
+# galery_1 = Galery.create(user: sabina, name: "Da Silva", location: "Paris")
+# galery_2 = Galery.create(user: sabina, name: "Templon", location: "Paris")
+# galery_3 = Galery.create(user: sabina, name: "Almine Reich", location: "Berlin")
+# galery_4 = Galery.create(user: sabina, name: "Da Silva", location: "Brussels")
+
+# exhibition_1 = Exhibition.create(galery: galery_1, name: Faker::Name.name, start_date: date, end_date: date, artist: artist, user:sabina, galery: galery_1)
+
+# showroom_1 = Showroom.create(exhibition:exhibition_1, name: Faker::Name)
+# showroom_2 = Showroom.create(exhibition: exhibition_2, name: Faker::Name)
+# showroom_3 = Showroom.create(exhibition: exhibition_3, name: Faker::Name)
+# showroom_4 = Showroom.create(exhibition: exhibition_4, name: Faker::Name)
+
+
+
